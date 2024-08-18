f048744ffefb1ff7512d0dbb21f3bd6deaaae9a2
Mohamed Ashif K M <mohamedashif1911@gmail.com>
2024-08-18 22:42:07 +0530

    added env

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+require('dotenv').config();
 
 import postRoutes from './routes/posts.js'
 
@@ -14,7 +15,7 @@ app.use(cors());
 
 app.use('/posts',postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://ashif:ua64XcQRGoW8lSN1@cluster0.ayx1z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const CONNECTION_URL = process.env.MONGO_URI
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
